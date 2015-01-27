2036746588f9545faa25890d462590ed25514b4e
vsharma2013 <vishal.sharma@talentica.com>
2015-01-27 17:39:27 +0530

    OO implementation

--- nodeCrawler.js
+++ nodeCrawler.js
@@ -1,19 +1,31 @@
 
 var fs = require('fs');
-var connStr = 'mongodb://192.168.50.213:27017/db1';
 var mongoClient = require('mongodb').MongoClient;
-var dbHandle = null;
 var env = require('jsdom').env;
-var rootDir = 'https://www.valueresearchonline.com/funds/newsnapshot.asp?schemecode=';
-var htmlFiles = ['10563',  '11463',  '2142',  '2599',  '2645',  '3019',  '3087',  '5069',  '6778'];
 
-function processHTML(htmlFileName, cbOnDone){
-  env(htmlFileName, function(errors, window){
-    onHtmlLoad(errors, window, htmlFileName, cbOnDone)
-  });
+function CrawlApp(){
+  this.dbConnString = 'mongodb://192.168.50.213:27017/db1';
+  this.dbHandle = null;
+  this.targetRootUrl = 'https://www.valueresearchonline.com/funds/newsnapshot.asp?schemecode=';
+  this.targetIDs = ['10563',  '11463',  '2142',  '2599',  '2645',  '3019',  '3087',  '5069',  '6778'];
+  this.allData = [];
+  this.processedCount = 0;
 }
 
-function onHtmlLoad(errors, window,htmlFileName, cbOnDone){
+CrawlApp.prototype.run = function(){
+  this.targetIDs.forEach((function(id){
+    var targetUrl = this.targetRootUrl + id;
+    this.processUrl(targetUrl);
+  }).bind(this));
+}
+
+CrawlApp.prototype.processUrl = function(targetUrl){
+  env(targetUrl, (function(errors, window){
+    this.onUrlResponse(errors, window, targetUrl)
+  }).bind(this));
+}
+
+CrawlApp.prototype.onUrlResponse = function(errors, window, targetUrl){
   var $ = require('jquery')(window);
   var $cols1 = $(".margin_top15px table").last().find("td.align_right");
   var $cols2 = $(".margin_top15px table").last().find("a");
@@ -21,15 +33,15 @@ function onHtmlLoad(errors, window,htmlFileName, cbOnDone){
   var vals1 = [];
   $.each($cols1, function(i,c){   vals1.push($(c).text()) });
   var vals2 = [];
-  $.each($cols2, function(i,c){ vals2.push($(c).text()) });
+  $.each($cols2, function(i,c){   vals2.push($(c).text()) });
 
   var jVals1 = [];
   for(var i = 0 ; i < vals1.length; i+=4){
     var obj = {
-      fileName : htmlFileName,
-      pe : parseFloat(vals1[i]),
-      high3yr : parseFloat(vals1[i+1]),
-      low3yr : parseFloat(vals1[i+2]),
+      url           : targetUrl,
+      pe            : parseFloat(vals1[i]),
+      high3yr       : parseFloat(vals1[i+1]),
+      low3yr        : parseFloat(vals1[i+2]),
       percentAssets : parseFloat(vals1[i+3])    
     }
     jVals1.push(obj);
@@ -39,44 +51,42 @@ function onHtmlLoad(errors, window,htmlFileName, cbOnDone){
     jVals1[i].company = vals2[j]; j++;
     jVals1[i].sector = vals2[j]; j++;
   }
-  cbOnDone(jVals1);
+  this.onPostUrlProcess(jVals1);  
 }
 
-var allData = [];
-var processedCount = 0;
-
-function onHtmlProcessed(data){
-    allData = allData.concat(data);
-    processedCount++;
-    console.log('Processed count : ' +  processedCount);
-    if(processedCount >= htmlFiles.length){
-      addRecordsToDb();
-    }
+CrawlApp.prototype.onPostUrlProcess = function(dataArray){
+  this.allData = this.allData.concat(dataArray);
+  this.processedCount++;
+  console.log('Processed count : ' +  this.processedCount);
+  if(this.processedCount >= this.targetIDs.length){
+    this.addRecordsToDb();
+  }  
 }
 
-htmlFiles.forEach(function(htmlFile) {
-    processHTML(rootDir + htmlFile, onHtmlProcessed);
-});
-
-function addRecordsToDb(){
-  mongoClient.connect(connStr, function(err, db){
+CrawlApp.prototype.addRecordsToDb = function(){
+  mongoClient.connect(this.dbConnString, (function(err, db){
     if(err) {
       console.log(err);
       return;
     }
-    dbHandle = db;
-    insertRecords(allData);
-  });
+    this.dbHandle = db;
+    this.insertRecords();
+  }).bind(this));
 }
 
-function insertRecords(records){
-  dbHandle.collection('valueResearch').insert(records, {safe:true}, function(err, result){
+CrawlApp.prototype.insertRecords = function(){
+  var records = this.allData;
+  this.dbHandle.collection('valueResearch').insert(records, {safe:true}, function(err, result){
     if(err){
       console.log(err);
       return ;
     }
     else{
-      console.log('RecordsAddedecords added successfully count = ' + records.length);
+      console.log('Added successfully count = ' + records.length);
     }
   });
 }
+
+
+var crawlApp = new CrawlApp();
+crawlApp.run();
